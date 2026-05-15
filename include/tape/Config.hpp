ca1a0e4fca34cbcb4ab0116b327442aab1752513
David Cherednik <david.cherednik@gmail.com>
2026-05-15 13:05:03 +0300

    work on examples in README, settle on the default config

--- include/tape/Config.hpp
+++ include/tape/Config.hpp
@@ -2,10 +2,10 @@
 
 #include <chrono>
 
-using Duration = std::chrono::nanoseconds;
+using Duration = std::chrono::milliseconds;
 
 struct TapeConfig {
   Duration ReadDelay{10};
-  Duration WriteDelay{10};
+  Duration WriteDelay{20};
   Duration MoveDelay{5};
 };
--- src/filetape.cpp
+++ src/filetape.cpp
@@ -1,48 +1,9 @@
 #include <format>
-#include <limits>
-#include <numeric>
 #include <print>
-#include <random>
 
 #include <lyra/lyra.hpp>
 
 #include "tape/FileTape.hpp"
-#include "tape/ITape.hpp"
-
-struct RandCommand {
-  uint64_t size{30};
-  std::string path;
-
-  void add_command(lyra::group &g) { // NOLINT
-    g.add_argument(
-        lyra::command("rand",
-                      [this](lyra::group const &f) {
-                        this->do_command(f);
-                      })
-            .help(std::format("Generate random tape with specified size (1 cell = {} bytes)",
-                              sizeof(TapeCell)))
-            .add_argument(lyra::arg(path, "out.bin").required())
-            .add_argument(lyra::arg(size, "size").required()));
-  }
-
-  void do_command(lyra::group const &g) { // NOLINT
-    auto tape{FileTape::CreateNew({}, path, size)};
-    if (!tape) {
-      std::cerr << tape.error() << '\n';
-      return;
-    }
-
-    std::random_device rd;
-    std::mt19937 gen{rd()};
-    // std::uniform_int_distribution<TapeCell> dist{std::numeric_limits<TapeCell>::min(),
-    //                                              std::numeric_limits<TapeCell>::max()};
-    std::uniform_int_distribution<TapeCell> dist{-1000, 1000};
-
-    do {
-      tape->Write(dist(gen));
-    } while (tape->MoveRight());
-  }
-};
 
 struct PrintCommand {
   std::string path;
@@ -53,7 +14,7 @@ struct PrintCommand {
                                    this->do_command(f);
                                  })
                        .help("Print the file tape")
-                       .add_argument(lyra::arg(path, "in.bin").required()));
+                       .add_argument(lyra::arg(path, "tape.bin").required()));
   }
 
   void do_command(lyra::group const &g) { // NOLINT
@@ -78,9 +39,6 @@ int main(int argc, char const **argv) {
 
   lyra::group subcommands;
   subcommands.require(1, 1);
-
-  RandCommand rand;
-  rand.add_command(subcommands);
 
   PrintCommand print;
   print.add_command(subcommands);
